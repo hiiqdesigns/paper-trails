f680de12517a7ed56a9568be5b4c6e3ad94d9303
HI IQ Designs <hiiqdesigns@gmail.com>
2024-03-20 09:32:23 -0500

    added next 5 states and updated stateList data model and import structure

--- app/components/US-Map/stateList.js
+++ app/components/US-Map/stateList.js
@@ -1,13 +1,27 @@
-import { Alabama } from "./States/Alabama";
-import { Alaska } from "./States/Alaska";
-import { Arkansas } from "./States/Arkansas";
-import { Arizona } from "./States/Arizona";
-
-const states = require.context("./States");
+import Alabama from "./States/Alabama";
+import Alaska from "./States/Alaska";
+import Arkansas from "./States/Arkansas";
+import Arizona from "./States/Arizona";
+import California from "./States/California";
+import Colorado from "./States/Colorado";
+import Delaware from "./States/Delaware";
+import Florida from "./States/Florida";
+import Georgia from "./States/Georgia";
+import Hawaii from "./States/Hawaii";
 
 export const stateList = [
-  { id: "AL", name: "Alabama", component: <Alabama /> },
-  { id: "AK", name: "Alaska", component: <Alaska /> },
-  { id: "AR", name: "Arkansas", component: <Arkansas /> },
-  { id: "AZ", name: "Arizona", component: <Arizona /> },
+  { id: "AL", name: "Alabama", component: <Alabama key={"state-AL"} /> },
+  { id: "AK", name: "Alaska", component: <Alaska key={"state-AK"} /> },
+  { id: "AR", name: "Arkansas", component: <Arkansas key={"state-AR"} /> },
+  { id: "AZ", name: "Arizona", component: <Arizona key={"state-AZ"} /> },
+  {
+    id: "CA",
+    name: "California",
+    component: <California key={"state-CA"} />,
+  },
+  { id: "CO", name: "Colorado", component: <Colorado key={"state-CO"} /> },
+  { id: "DE", name: "Delaware", component: <Delaware key={"state-DE"} /> },
+  { id: "FL", name: "Florida", component: <Florida key={"state-FL"} /> },
+  { id: "GA", name: "Georgia", component: <Georgia key={"state-GA"} /> },
+  { id: "HI", name: "Hawaii", component: <Hawaii key={"state-HI"} /> },
 ];
