0dad1005f34f2d1c2ecf27c4061964924adbf13a
HI IQ Designs <hiiqdesigns@gmail.com>
2024-03-20 10:01:49 -0500

    added next 5 states (11-15)

--- app/components/US-Map/stateList.js
+++ app/components/US-Map/stateList.js
@@ -1,19 +1,24 @@
 import Alabama from "./States/Alabama";
 import Alaska from "./States/Alaska";
-import Arkansas from "./States/Arkansas";
 import Arizona from "./States/Arizona";
+import Arkansas from "./States/Arkansas";
 import California from "./States/California";
 import Colorado from "./States/Colorado";
 import Delaware from "./States/Delaware";
 import Florida from "./States/Florida";
 import Georgia from "./States/Georgia";
 import Hawaii from "./States/Hawaii";
+import Idaho from "./States/Idaho";
+import Illinois from "./States/Illinois";
+import Indiana from "./States/Indiana";
+import Iowa from "./States/Iowa";
+import Kansas from "./States/Kansas";
 
 export const stateList = [
   { id: "AL", name: "Alabama", component: <Alabama key={"state-AL"} /> },
   { id: "AK", name: "Alaska", component: <Alaska key={"state-AK"} /> },
-  { id: "AR", name: "Arkansas", component: <Arkansas key={"state-AR"} /> },
   { id: "AZ", name: "Arizona", component: <Arizona key={"state-AZ"} /> },
+  { id: "AR", name: "Arkansas", component: <Arkansas key={"state-AR"} /> },
   {
     id: "CA",
     name: "California",
@@ -24,4 +29,9 @@ export const stateList = [
   { id: "FL", name: "Florida", component: <Florida key={"state-FL"} /> },
   { id: "GA", name: "Georgia", component: <Georgia key={"state-GA"} /> },
   { id: "HI", name: "Hawaii", component: <Hawaii key={"state-HI"} /> },
+  { id: "ID", name: "Idaho", component: <Idaho key={"state-ID"} /> },
+  { id: "IL", name: "Illinois", component: <Illinois key={"state-IL"} /> },
+  { id: "IN", name: "Indiana", component: <Indiana key={"state-IN"} /> },
+  { id: "IA", name: "Iowa", component: <Iowa key={"state-IA"} /> },
+  { id: "KS", name: "Kansas", component: <Kansas key={"state-KS"} /> },
 ];
