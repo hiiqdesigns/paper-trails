b94a597561044714ea56120dff9e3098ab349967
HI IQ Designs <hiiqdesigns@gmail.com>
2024-03-21 12:33:42 -0500

    updated state design on hover

--- app/components/US-Map/index.tsx
+++ app/components/US-Map/index.tsx
@@ -1,15 +1,23 @@
 "use client";
+import classNames from "classnames";
 import { stateList } from "./stateList.js";
 
 export const USMap = () => {
   return (
     <svg viewBox="0 0 1386 822" id="us-map">
       {stateList.map((state) => {
+        const hoverColor = classNames({
+          "hover:fill-[#D00037]": state.stateControl == "Rep",
+          "hover:fill-[#0048A0]": state.stateControl == "Dem",
+          "hover:fill-[#68246C]": state.stateControl == "Divided",
+          "hover:fill-slate-400": state.stateControl == null,
+        });
+
         return (
           <g
             key={`${state.id}-${state.name}`}
             onClick={() => alert(state.name)}
-            className="fill-slate-200 stroke-black stroke-[0.125px] hover:fill-slate-400"
+            className={`hover:cursor-pointer stroke-black stroke-[0.125px] fill-slate-200 ${hoverColor}`}
           >
             {state.component()}
           </g>
