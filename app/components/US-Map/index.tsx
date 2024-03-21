8855af834c0620286ee709b4589af191375a6d1a
HI IQ Designs <hiiqdesigns@gmail.com>
2024-03-21 11:47:08 -0500

    updated map initial design and added hover and test click behavior

--- app/components/US-Map/index.tsx
+++ app/components/US-Map/index.tsx
@@ -1,10 +1,19 @@
+"use client";
 import { stateList } from "./stateList.js";
 
 export const USMap = () => {
   return (
     <svg viewBox="0 0 1386 822" id="us-map">
       {stateList.map((state) => {
-        return state.component;
+        return (
+          <g
+            key={`${state.id}-${state.name}`}
+            onClick={() => alert(state.name)}
+            className="fill-slate-200 stroke-black stroke-[0.125px] hover:fill-slate-400"
+          >
+            {state.component()}
+          </g>
+        );
       })}
     </svg>
   );
