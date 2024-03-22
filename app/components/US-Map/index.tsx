2a53171b4ee91fda159f36cafd67ba9a99555d58
HI IQ Designs <hiiqdesigns@gmail.com>
2024-03-21 19:32:49 -0500

    added useRouter to change url on state click

--- app/components/US-Map/index.tsx
+++ app/components/US-Map/index.tsx
@@ -1,8 +1,11 @@
 "use client";
 import classNames from "classnames";
+import { useRouter } from "next/navigation.js";
 import { stateList } from "./stateList.js";
 
 export const USMap = () => {
+  const router = useRouter();
+
   return (
     <svg viewBox="0 0 1386 822" id="us-map">
       {stateList.map((state) => {
@@ -16,7 +19,7 @@ export const USMap = () => {
         return (
           <g
             key={`${state.id}-${state.name}`}
-            onClick={() => alert(state.name)}
+            onClick={() => router.push(`/${state.name}`)}
             className={`hover:cursor-pointer stroke-black stroke-[0.125px] fill-slate-200 ${hoverColor}`}
           >
             {state.component()}
