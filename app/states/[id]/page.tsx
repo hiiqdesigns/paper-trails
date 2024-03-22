20ef6bf4b8de2b8453c035cf5836728d5b27bab0
HI IQ Designs <hiiqdesigns@gmail.com>
2024-03-22 04:14:09 -0500

    each state returns api data on click

--- app/states/[id]/page.tsx
+++ app/states/[id]/page.tsx
@@ -1,6 +1,16 @@
-import { stateList } from "../../components/US-Map/stateList";
+const openSecretsAPIKey = process.env.OPENSECRETS_API_KEY;
 
-export default function State({
+async function getData(state: string) {
+  const res = await fetch(
+    `https://www.opensecrets.org/api/?method=getLegislators&id=${state}&apikey=${openSecretsAPIKey}&output=json`
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+export default async function State({
   params,
 }: {
   params: {
@@ -8,13 +18,24 @@ export default function State({
   };
 }) {
   const { id } = params;
-  console.log(stateList.find((state) => state.id.toLowerCase() == id));
+  const data = await getData(id);
+  const legislatorsList = data.response.legislator;
+
   return (
     <main className="flex flex-col items-center min-h-screen">
       <section className="w-full max-w-screen-xl">
         {params.id}
         <br />
         Welcome to the {params.id} page!
+        <ul>
+          {legislatorsList.map((legislator: any) => {
+            return (
+              <li key={legislator["@attributes"].cid}>
+                <span>{legislator["@attributes"].firstlast}</span>
+              </li>
+            );
+          })}
+        </ul>
       </section>
     </main>
   );
