3d1341033f3b2c7702203f0026ca6fc37c271f3b
HI IQ Designs <hiiqdesigns@gmail.com>
2024-03-24 00:48:09 -0500

    updated candidate data cycle

--- utils/getOpenSecretsData.js
+++ utils/getOpenSecretsData.js
@@ -12,7 +12,7 @@ export default async function getLegisData(state) {
 
 export async function getCandData(id) {
   const res = await fetch(
-    `https://www.opensecrets.org/api/?method=candSummary&cid=${id}&cycle=2022&apikey=${openSecretsAPIKey}&output=json`
+    `https://www.opensecrets.org/api/?method=candSummary&cid=${id}&cycle=2024&apikey=${openSecretsAPIKey}&output=json`
   );
   if (!res.ok) {
     throw new Error("Failed to fetch data");
