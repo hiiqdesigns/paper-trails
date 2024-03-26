d39ec5f081bc7742c5e0b327338a36aea1711666
HI IQ Designs <hiiqdesigns@gmail.com>
2024-03-26 01:54:40 -0500

    added app logo and go vote button

--- app/components/ui/navbar.tsx
+++ app/components/ui/navbar.tsx
@@ -12,18 +12,18 @@ export const Navbar = () => {
         isOpen ? " bg-white md:bg-transparent" : " bg-transparent"
       }`}
     >
-      <div className="container px-8 py-3 mx-auto md:flex">
+      <div className="container px-8 py-3 mx-auto flex">
         <div className="flex items-center justify-between">
-          <Link href={`/`}>
+          <Link href={`/`} className="flex gap-2">
             <Image
               className="w-auto h-6 sm:h-7"
-              src="https://merakiui.com/images/full-logo.svg"
-              alt=""
+              src="/paper-trail-logo.png"
+              alt="Paper Trails Logo"
               width={100}
               height={60}
             />
+            Paper Trails
           </Link>
-
           {/* <!-- Mobile menu button --> */}
           {/* <div className="flex md:hidden">
             <button
@@ -64,6 +64,12 @@ export const Navbar = () => {
             </button>
           </div> */}
         </div>
+
+        <Link href={`https://www.vote.org`} target="_blank" className="ml-auto">
+          <button className="px-10 py-2 text-sm font-medium leading-5 text-center text-white capitalize bg-blue-600 rounded-lg hover:bg-blue-500 lg:mx-0 lg:w-auto focus:outline-none">
+            Go Vote!
+          </button>
+        </Link>
 
         {/* <!-- Mobile Menu open: "block", Menu closed: "hidden" --> */}
         {/* <div
